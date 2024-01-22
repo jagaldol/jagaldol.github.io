73bfd1c104a3f3e21258558368723e1f82085f6d
jagaldol <yoy06056@pusan.ac.kr>
2024-01-22 18:12:04 +0900

    gatsby 세팅

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    `./src/pages/**/*.{js,jsx,ts,tsx}`,
+    `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
