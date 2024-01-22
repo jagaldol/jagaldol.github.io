7364e2d669f47eebd52b1364c066b49535e367cf
jagaldol <yoy06056@pusan.ac.kr>
2024-01-23 01:58:12 +0900

    feat: Add cname plugin
    깃헙 배포 시 커스텀 도메인을 위해 추가

--- app/gatsby-config.ts
+++ app/gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from 'gatsby'
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `app`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://jagaldol.com/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -23,6 +23,7 @@ const config: GatsbyConfig = {
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    `gatsby-plugin-cname`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
