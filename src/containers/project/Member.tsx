5eda703c836ce87daf02f3474ee85ae4c32a76bd
jagaldol <yoy06056@pusan.ac.kr>
2024-02-13 05:23:12 +0900

    feat: 챗 푸디 작성 완료

--- src/containers/project/Member.tsx
+++ src/containers/project/Member.tsx
@@ -4,7 +4,7 @@ export default function Member({ member }: { member: MemberType }) {
   return (
     <>
       <div className="min-w-32 w-32 max-md:min-w-20 max-md:w-20 justify-self-end">
-        <img className="rounded-full" src={member.imageSrc} alt={`${member.name} 사진`} />
+        <img className="rounded-full border-slate-300 border" src={member.imageSrc} alt={`${member.name} 사진`} />
       </div>
       <div className="flex flex-col">
         <p className="text-xl max-md:text-base font-semibold my-2">{member.name}</p>
