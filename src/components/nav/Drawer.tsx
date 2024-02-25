110bafb09c1fdbdb71dea90b19c1370b5415165c
jagaldol <yoy06056@pusan.ac.kr>
2024-02-25 16:03:54 +0900

    feat: 메뉴 버튼 애니메이션 적용

--- src/components/nav/Drawer.tsx
+++ src/components/nav/Drawer.tsx
@@ -1,17 +1,10 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faX } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 import { Link } from 'gatsby'
 
-export default function Drawer({ onClose }: { onClose: () => void }) {
+export default function Drawer() {
   return (
-    <div className="fixed left-0 top-0 w-full h-screen z-30 bg-black/10">
+    <div className="fixed left-0 top-0 w-full h-screen z-30 bg-black/10 flex justify-end">
       <div className="bg-bg w-[80%] h-full p-5 flex flex-col items-center">
-        <div className="w-full text-end">
-          <button type="button" aria-label="닫기" onClick={onClose}>
-            <FontAwesomeIcon icon={faX} size="lg" />
-          </button>
-        </div>
         <Link to="/" className="text-2xl mt-12 mb-20">
           Jagaldol
         </Link>
--- src/components/nav/Navigator.tsx
+++ src/components/nav/Navigator.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'gatsby'
 import Drawer from '@/components/nav/Drawer'
 
@@ -9,10 +7,31 @@ export default function Navigator() {
   return (
     <>
       <nav className="hidden max-md:block pt-5">
-        <button type="button" aria-label="메뉴" onClick={() => setDrawer(true)}>
-          <FontAwesomeIcon icon={faBars} size="lg" />
+        <button
+          type="button"
+          aria-label="메뉴"
+          onClick={() => setDrawer(!drawer)}
+          className="fixed top-5 right-5 h-6 p-1 z-50"
+        >
+          {drawer ? (
+            <div
+              className="relative w-6 h-[3px] bg-transparent
+              transition-all duration-500 before:transition-all before:duration-500 after:transition-all after:duration-500
+              before:w-6 before:h-[3px] before:absolute before:bg-main-theme before:left-0
+              before:-rotate-45 before:top-0
+              after:w-6 after:h-[3px] after:absolute after:bg-main-theme after:left-0
+              after:rotate-45 after:top-0"
+            />
+          ) : (
+            <div
+              className="relative w-6 h-[3px] bg-main-theme
+              transition-all duration-500 before:transition-all before:duration-500 after:transition-all after:duration-500
+              before:w-6 before:h-[3px] before:absolute before:bg-main-theme before:top-[9px] before:left-0
+              after:w-6 after:h-[3px] after:absolute after:bg-main-theme after:-top-[9px] after:left-0"
+            />
+          )}
         </button>
-        {drawer ? <Drawer onClose={() => setDrawer(false)} /> : null}
+        {drawer ? <Drawer /> : null}
       </nav>
       <nav className="w-full px-9 py-10 flex items-center max-md:px-0 max-md:hidden">
         <div className="mx-10 flex-1">
