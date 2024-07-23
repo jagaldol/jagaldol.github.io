271378848c7cefec57450b8c9822ebaf473a402e
안혜준 <yoy06056@pusan.ac.kr>
2024-07-23 23:27:00 +0900

    feat: 졸업일자 변경

--- src/containers/home/profile/Profile.tsx
+++ src/containers/home/profile/Profile.tsx
@@ -82,7 +82,7 @@ export default function Profile() {
         <ProfileContent title="Education">
           <Paragraph content="구글 머신러닝 부트캠프 2024" description="2024.07. ~ 2024.10." />
           <Paragraph content="카카오 테크 캠퍼스 1기" description="2023.04. ~ 2023.11." />
-          <Paragraph content="부산대학교 정보컴퓨터공학부" description="2018.03. ~ 2024.08.">
+          <Paragraph content="부산대학교 정보컴퓨터공학부" description="2018.03. ~ 2025.02.">
             <p className={paragraphDescriptionClassName}>학점: 4.00 / 4.5</p>
           </Paragraph>
         </ProfileContent>
